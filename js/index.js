da407b98a600f1bb95171ad211110cca1312035b
adwenpa <joojo.abr@gmail.com>
2025-01-10 22:04:39 -0500

    eperimented loops, break and continue in index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+// let x = 0;
+// let z = 0;
+// labelCancelLoops: while (true) {
+//   console.log("Outer loops:", x);
+//   x += 1;
+//   z = 1;
+//   while (true) {
+//     console.log("Inner loops:", z);
+//     z += 1;
+//     if (z === 10 && x === 10) {
+//       break labelCancelLoops;
+//     } else if (z === 10) {
+//       break;
+//     }
+//   }
+// }
+
+// let i = 0;
+// let n = 0;
+// while (i < 5) {
+//   i++;
+//   n += i;
+//   console.log(n);
+// }
+
+// let i = 0;
+// let n = 0;
+// while (i < 5) {
+//   i++;
+//   if (i === 3) {
+//     continue;
+//   }
+//   n += i;
+//   console.log(n);
+// }
